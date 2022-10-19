d56b9abef40ddde5b0618d62fff7bc9568d743b4
Sasaki <104120192+katann-tann@users.noreply.github.com>
2022-10-19 11:13:41 +0900

    プレイヤー移動

--- Assets/Yoketoru/Scripts/Player.cs
+++ Assets/Yoketoru/Scripts/Player.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("スピード"), SerializeField]
+    float speed = 15;
+
+    float cameraDistance;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         var mpos = Input.mousePosition;
-        Debug.Log(mpos);
+        //Debug.Log(mpos);
+        mpos.z = cameraDistance;
+        var wp = Camera.main.ScreenToWorldPoint(mpos);
+
+        //　to = 現在地から目的地wpへの向きと大きさ(ベクトル)
+        Vector3 to = wp - transform.position;
+
+       
+
+        // to.magnitude=toの長さ
+        if(to.magnitude < 0.01f)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            float dist = Mathf.Min(to.magnitude, step);
+            float v = dist / Time.deltaTime;
+            rb.velocity = v * to.normalized;
+        }
+        //transform.position = wp;
     }
 }
